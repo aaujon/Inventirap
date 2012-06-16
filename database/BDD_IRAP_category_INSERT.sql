4085be933b67fbec1e1b2e4b8a0b64ada62c81e2
Stephane P <steph@100pui.com>
2012-06-16 19:01:34 +0200

    Update to database with INSERT of all category

---
+++ database/BDD_IRAP_category_INSERT.sql
@@ -0,0 +1,45 @@
+/*
+-- Query: 
+-- Date: 2012-06-16 18:55
+*/
+INSERT INTO `category` (`id_category`,`name`) VALUES (1,'Multimetre');
+INSERT INTO `category` (`id_category`,`name`) VALUES (2,'Oscilloscope');
+INSERT INTO `category` (`id_category`,`name`) VALUES (3,'Mesure simple');
+INSERT INTO `category` (`id_category`,`name`) VALUES (4,'Générateur de fonctions');
+INSERT INTO `category` (`id_category`,`name`) VALUES (5,'Atténuateur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (6,'Amplificateur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (7,'RF/Hyperfréquence');
+INSERT INTO `category` (`id_category`,`name`) VALUES (8,'Champs magnétique');
+INSERT INTO `category` (`id_category`,`name`) VALUES (9,'Analyseur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (10,'Sonde');
+INSERT INTO `category` (`id_category`,`name`) VALUES (11,'Effaceur de mémoire');
+INSERT INTO `category` (`id_category`,`name`) VALUES (12,'Programmateur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (13,'Balance');
+INSERT INTO `category` (`id_category`,`name`) VALUES (14,'Longeur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (15,'Angle');
+INSERT INTO `category` (`id_category`,`name`) VALUES (16,'Extensometrie');
+INSERT INTO `category` (`id_category`,`name`) VALUES (17,'Couple');
+INSERT INTO `category` (`id_category`,`name`) VALUES (18,'Accélération');
+INSERT INTO `category` (`id_category`,`name`) VALUES (19,'Fluxmetre');
+INSERT INTO `category` (`id_category`,`name`) VALUES (20,'Laser');
+INSERT INTO `category` (`id_category`,`name`) VALUES (21,'Analyseur de faisceau laser');
+INSERT INTO `category` (`id_category`,`name`) VALUES (22,'Optomètre');
+INSERT INTO `category` (`id_category`,`name`) VALUES (23,'Source optique');
+INSERT INTO `category` (`id_category`,`name`) VALUES (24,'Mesure du temps');
+INSERT INTO `category` (`id_category`,`name`) VALUES (25,'Composant étalon');
+INSERT INTO `category` (`id_category`,`name`) VALUES (26,'Pression');
+INSERT INTO `category` (`id_category`,`name`) VALUES (27,'Température');
+INSERT INTO `category` (`id_category`,`name`) VALUES (28,'Contamination');
+INSERT INTO `category` (`id_category`,`name`) VALUES (29,'Hygrometrie');
+INSERT INTO `category` (`id_category`,`name`) VALUES (30,'Débit');
+INSERT INTO `category` (`id_category`,`name`) VALUES (31,'Centrale et data logger');
+INSERT INTO `category` (`id_category`,`name`) VALUES (32,'Carte d''aquisition PC');
+INSERT INTO `category` (`id_category`,`name`) VALUES (33,'Alimentation');
+INSERT INTO `category` (`id_category`,`name`) VALUES (34,'Transformateur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (35,'Station de soudage');
+INSERT INTO `category` (`id_category`,`name`) VALUES (36,'Bobineuse');
+INSERT INTO `category` (`id_category`,`name`) VALUES (37,'Compresseur');
+INSERT INTO `category` (`id_category`,`name`) VALUES (38,'Enceinte à vide');
+INSERT INTO `category` (`id_category`,`name`) VALUES (39,'Etuve/Enceinte thermique');
+INSERT INTO `category` (`id_category`,`name`) VALUES (40,'Pompes');
+INSERT INTO `category` (`id_category`,`name`) VALUES (41,'Matériel informatique en prêt');

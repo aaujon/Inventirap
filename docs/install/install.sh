5c01923a8ff9c64d903b183d9b607b3b073ac30b
Stephane P <steph@100pui.com>
2012-07-03 14:28:26 +0200

    Adding new rule for textfiel validation (REGEX to improve) #47 + Small changes (docs)

--- docs/install/install.sh
+++ docs/install/install.sh
@@ -10,9 +10,7 @@ git clone git://github.com/aaujon/Inventirap.git
 #Configurer l'application
 cd Inventirap/
 chmod -R 777 cakephp/app/tmp/
-chmod 777 cakephp/app/Vendor/phpqrcode/error.txt
+chmod 777 cakephp/app/Vendor/phpqrcode/errors.txt
 
 #Mettre à jour la bdd
 mysql -u root -p < database/BDD_IRAP.sql
-
-#ENCORE NON TESTE !!

a066bed2e23cc1d2167a3e0fce5db8eccb5ba0c7
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-06 20:24:41 +0200

    Rename the web service in french

---
+++ cakephp/app/Controller/ServicesWebController.php
@@ -0,0 +1,23 @@
+<?php
+
+//App::import('Controller', 'Materials');
+
+
+class ServicesWebController extends AppController {
+	
+	var $layout = 'xml';
+	
+	public function beforeFilter() {
+				$this->LdapAuth->allow('*');
+	}
+	
+	public function materiel($id = null) {
+		$mat = ClassRegistry::init('Material');
+		
+		$mat->id = $id;
+		
+		$this->set('materials', $mat->read());
+	}
+}
+
+?>

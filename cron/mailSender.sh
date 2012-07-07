dada4cb2e2d1b2900a3d3e381b1ff860fea3b0ae
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-07 17:25:51 +0200

    Develop the cron script

--- cron/mailSender.sh
+++ cron/mailSender.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "plop";
+export userBddName="root";
+export userBddPassword="root";
+export bddName="mydb";
+
+echo "select UNIX_TIMESTAMP(date_prochain_controle), UNIX_TIMESTAMP(NOW()), materiel_id from suivis" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > logSuivis.txt
+
+cat logSuivis.txt | cut -d'	' -f1

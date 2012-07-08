2c6114f638797b1d8f0b3c3997767195cb806fed
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-08 18:37:42 +0200

    Fix bugs into the install script

--- install/install.sh
+++ install/install.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
-#Aller dans le repertoire du serveur web
-cd /var/www/
+# Parametres à changer pour votre installation
+export bddUserName="root";
+export bddPassword="root";
+export bddName="mydb"
 
 #Mettre à jour les sources
 echo "Récupération des sources..."
-#rm -rf Inventirap/
-#git clone git://github.com/aaujon/Inventirap.git
+# rm -rf Inventirap/
+# git clone git://github.com/aaujon/Inventirap.git
 
 #Configurer l'application
 echo "Mise à jour des droits des dossiers..."
-#cd Inventirap/
-#chmod -R 777 cakephp/app/tmp/
-#chmod 777 cakephp/app/Vendor/phpqrcode/errors.txt
+cd Inventirap/
+chmod -R 777 ./cakephp/app/tmp/
+chmod 777 ./cakephp/app/Vendor/phpqrcode/errors.txt
 
 # Set database ip
-echo "Database ip"
+echo "Entrez l'adresse du serveur MySQL"
 read bddIp
-sed "s/192.168.1.70/$bddIp/" /var/www/Inventirap/cakephp/app/Config/database.php
+sed "s/192.168.1.70/$bddIp/" ./cakephp/app/Config/database.php
 
 #Mettre à jour la bdd
 echo "Mise à jour de la BDD..."
-mysql -u root -p < /var/www/Inventirap/database/BDD_IRAP.sql
+mysql -u $bddUserName --password=$bddPassword -h $bddIp $bddName < ./database/BDD_IRAP.sql
 
 #Ajouter Super Administrateur
-echo "Super Administrateur ?"
-read superAdmin
-mysql -u root -p -D mydb -e "INSERT INTO special_users (id, ldap, role) VALUES ('', '$superAdmin', 'Super Administrator');"
+echo "Quel est le nom du super administrateur de l'inventaire"
+read superAdminName
+echo "Quel est l'adresse email du super administrateur de l'inventaire"
+read superAdminEmail
+mysql -u $bddUserName --password=$bddPassword -h $bddIp -D $bddName -e "INSERT INTO utilisateurs (ldap, role, email) VALUES ('$superAdminName', 'Super Administrator', '$superAdminEmail');"
 
 # Set LDAP address
-echo "LDAP Address"
+echo "Entrez l'adresse du serveur ldap"
 read ldapIp
-sed "s/192.168.1.65/$ldapIp/" /var/www/Inventirap/cakephp/app/Config/database.php
+sed "s/192.168.1.65/$ldapIp/" ./cakephp/app/Config/database.php
 
 # Set LDAP port
-echo "LDAP port"
+echo "Entrez le numéro de port du serveur ldap"
 read ldapPort
-sed "s/389/$ldapPort/" /var/www/Inventirap/cakephp/app/Config/database.php
+sed "s/389/$ldapPort/" ./cakephp/app/Config/database.php
 
 # Ajouter les droits en ecriture pour la creation des qrcodes
-chmod -R 777 /var/www/Inventirap/cakephp/app/Vendor/phpqrcode/test-errors.txt
-chmod -R 777 /var/www/Inventirap/cakephp/app/Vendor/phpqrcode/errors.txt
-mkdir /var/www/Inventirap/cakephp/app/tmp/qrcodes
-chmod -R 777 /var/www/Inventirap/cakephp/app/tmp/qrcodes/
+touch ./cakephp/app/Vendor/phpqrcode/test-errors.txt
+chmod 777 ./cakephp/app/Vendor/phpqrcode/test-errors.txt
+touch ./cakephp/app/Vendor/phpqrcode/errors.txt
+chmod 777 ./cakephp/app/Vendor/phpqrcode/errors.txt
+mkdir ./cakephp/app/tmp/qrcodes
+chmod -R 777 ./cakephp/app/tmp/qrcodes/

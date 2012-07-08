de9d92f62355f01a60ff1824c644170dda8b87bd
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-08 16:27:46 +0200

    Best script to sendMail to the bdd

--- cron/mailSender.sh
+++ cron/mailSender.sh
@@ -8,54 +8,50 @@ export bddName="mydb";
 # Don't touch to the rest of the file !
 export file=logSuivis.txt;
 
-echo "SELECT email_responsable, designation from materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 2592000 AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) > 1296000" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > `echo $file`;
-
+# One month reminding
+echo "SELECT utilisateur_id, designation from materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 2592000 AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) > 1296000" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d' > $file;
 while IFS= read -r line; do
-    email=`echo $line | cut -d' ' -f1`;
-    designation=`echo $line | sed -e "s/$email//"` ;
-
+    utilisateur_id=`echo $line | cut -d' ' -f1`;
+    designation=`echo $line | sed -e "s/$utilisateur_id//"` ;
+    email=`echo "SELECT email from utilisateurs WHERE id = $utilisateur_id" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d'`
     echo "Il ne vous reste qu'un mois pour effectuer la maintenant du materiel : $designation. Email = $email";
 done < $file;
 
-
-echo "SELECT email_responsable, designation FROM materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 1296000;" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > `echo $file`;
-
+# Two weeks remindind
+echo "SELECT utilisateur_id, designation from materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 1296000 AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) > 864000" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d' > $file;
 while IFS= read -r line; do
-    email=`echo $line | cut -d' ' -f1`;
-    designation=`echo $line | sed -e "s/$email//"` ;
-
+    utilisateur_id=`echo $line | cut -d' ' -f1`;
+    designation=`echo $line | sed -e "s/$utilisateur_id//"` ;
+    email=`echo "SELECT email from utilisateurs WHERE id = $utilisateur_id" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d'`
     echo "Il ne vous reste deux semaines pour effectuer la maintenant du materiel : $designation. Email = $email";
 done < $file;
 
-
-echo "SELECT email_responsable, designation FROM materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 864000;" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > `echo $file`;
-
+# Ten days reminding
+echo "SELECT utilisateur_id, designation from materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 864000 AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) > 86400" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d' > $file;
 while IFS= read -r line; do
-    email=`echo $line | cut -d' ' -f1`;
-    designation=`echo $line | sed -e "s/$email//"` ;
-
+    utilisateur_id=`echo $line | cut -d' ' -f1`;
+    designation=`echo $line | sed -e "s/$utilisateur_id//"` ;
+    email=`echo "SELECT email from utilisateurs WHERE id = $utilisateur_id" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d'`
     echo "Il ne vous reste 10 jours pour effectuer la maintenant du materiel : $designation. Email = $email";
 done < $file;
 
-
-echo "SELECT email_responsable, designation FROM materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 86400;" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > `echo $file`;
-
+# One day reminding
+echo "SELECT utilisateur_id, designation from materiels INNER JOIN suivis ON materiels.id = suivis.materiel_id AND UNIX_TIMESTAMP(suivis.date_prochain_controle) - UNIX_TIMESTAMP(NOW()) <= 86400" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d' > $file;
 while IFS= read -r line; do
-    email=`echo $line | cut -d' ' -f1`;
-    designation=`echo $line | sed -e "s/$email//"` ;
-
+    utilisateur_id=`echo $line | cut -d' ' -f1`;
+    designation=`echo $line | sed -e "s/$utilisateur_id//"` ;
+    email=`echo "SELECT email from utilisateurs WHERE id = $utilisateur_id" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d'`
     echo "C'est aujourd'hui que vous devez effectuer la maintenant du materiel : $designation. Email = $email";
 done < $file;
 
-echo "SELECT email_responsable, designation FROM materiels WHERE UNIX_TIMESTAMP(date_acquisition) - UNIX_TIMESTAMP(NOW()) >= 157680000;" | mysql -u `echo $userBddName` --password=`echo $userBddPassword` `echo $bddName` | sed -e '1d' > `echo $file`;
-
+# Five years birthday
+echo "SELECT utilisateur_id, designation from materiels WHERE UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(date_acquisition) >= 157680000" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d' > $file;
 while IFS= read -r line; do
-    email=`echo $line | cut -d' ' -f1`;
-    designation=`echo $line | sed -e "s/$email//"` ;
-
-    echo "Il y a plus de cinq ans que le materiel $designation est entré dans l'inventaire. Voulez-vous le concerver ? Email = $email";
+    utilisateur_id=`echo $line | cut -d' ' -f1`;
+    designation=`echo $line | sed -e "s/$utilisateur_id//"` ;
+    email=`echo "SELECT email from utilisateurs WHERE id = $utilisateur_id" | mysql -u $userBddName --password=$userBddPassword $bddName | sed -e '1d'`
+    echo "Il y a plus de 5 ans que le materiel : $designation, est entre dans l'inventaire. Voulez-vous le concerver ? Email = $email";
 done < $file;
-
 
 # clean te directory
 rm $file;

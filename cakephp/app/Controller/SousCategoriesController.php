3a43039e0649068699d012c25be584d93205c6af
Stephane P <steph@100pui.com>
2012-07-10 19:22:55 +0200

    Resolved all Loan IHM improvement. Fixed #88

--- cakephp/app/Controller/SousCategoriesController.php
+++ cakephp/app/Controller/SousCategoriesController.php
@@ -3,7 +3,10 @@ class SousCategoriesController extends AppController {
     public $scaffold;
 
     public function getByCategory() {
-		$category_id = $this->request->data['Materiel']['category_id'];
+    	if (isset($this->request->data['Materiel']['s_category_id']))
+			$category_id = $this->request->data['Materiel']['s_category_id'];
+		else
+			$category_id = $this->request->data['Materiel']['category_id'];
 		$subcategories = $this->SousCategory->find('list', array(
 			'conditions' => array('SousCategory.category_id' => $category_id),
 			'order' => array('SousCategory.nom'),
--- cakephp/app/Model/Emprunt.php
+++ cakephp/app/Model/Emprunt.php
@@ -6,28 +6,67 @@ class Emprunt extends AppModel {
 	
 			
 	var $validate = array(
+		'materiel_id' => array(
+			'rule' => 'notEmpty',              
+			'message' => 'Le champ doit être rempli.'
+		),
 		'piece' => array(    
 			'valid' => array(            
-				'rule' => 'check_string',
-				'allowEmpty' => true,         
+				'rule' => 'check_string',         
 				'message' => 'Le champ doit être valide.'        
-			),      
-		),
-		'responsable' => array(    
-			'valid' => array(            
-				'rule' => 'check_string',
-				'allowEmpty' => true,              
-				'message' => 'Le champ doit être valide.'        
-			),      
+			),
+			'checkPieceLaboratoire' => array(
+				'rule' => 'checkPieceLaboratoire',
+				'message' => 'L\'un des deux champs suivant doit être renseigné: Pièce et/ou Laboratoire'
+			)
 		),
 		'laboratoire' => array(    
 			'valid' => array(            
-				'rule' => 'check_string',
-				'allowEmpty' => true,              
+				'rule' => 'check_string',         
+				'message' => 'Le champ doit être valide.'        
+			), 
+			'checkPieceLaboratoire' => array(
+				'rule' => 'checkPieceLaboratoire',
+				'message' => 'L\'un des deux champs suivant doit être renseigné: Pièce et/ou Laboratoire'
+			)    
+		),
+		'responsable' => array(    
+			'valid' => array(            
+				'rule' => 'check_string',            
 				'message' => 'Le champ doit être valide.'        
 			),      
+			'nonVide' => array(
+				'rule' => 'notEmpty',              
+				'message' => 'Le champ doit être rempli.'
+			)
 		),
-		
+		'date_retour_emprunt' => array(
+			'returnDateAfterLoanDate' => array(
+				'rule' => 'returnDateAfterLoanDate',
+				'message' => 'La date de retour doit être postérieure à la date de l\'emprunt.'
+			)
+		),
+		'date_emprunt' => array(
+			'rule' => 'notEmpty',              
+			'message' => 'Le champ doit être rempli.'
+		)
 	);
+	
+	function returnDateAfterLoanDate() { 
+		$emp = $this->data[$this->name]['date_emprunt'];
+		$emp = mktime(0, 0, 0, substr($emp, 5, 2), substr($emp, -2), substr($emp, 0, 4)); 
+		$ret = $this->data[$this->name]['date_retour_emprunt'];
+		$ret = mktime(0, 0, 0, substr($ret, 5, 2), substr($ret, -2), substr($ret, 0, 4));
+		if ($ret <= $emp)
+			return false;
+		return true;
+    }
+    
+    function checkPieceLaboratoire() { 
+		$emp = $this->data[$this->name]['piece'];
+		$ret = $this->data[$this->name]['laboratoire'];
+		
+		return false;
+    }
 }
 ?>

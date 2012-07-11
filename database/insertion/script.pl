0119145f8f0dea33236977f69ef6e16af7608d98
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-11 11:59:09 +0200

    Perl script to insert values for the raw csv

--- database/insertion/script.pl
+++ database/insertion/script.pl
@@ -2,10 +2,80 @@
 
 use DBI;
 use DBD::mysql;
+use List::MoreUtils qw(uniq);
+use Data::Dumper qw(Dumper);
 
-####
-# Insert SQL requests
-####
+sub formatMaterielAdministratif {
+
+	$technique = $_[0];
+	
+	if($technique eq "") {
+		return '1';
+	}
+		
+	return '0';
+}
+
+sub formatMaterielTechnique {
+
+	$technique = $_[0];
+	
+	if($technique eq "") {
+		return '0';
+	}
+		
+	return '1';
+}
+
+sub sqlRequest {
+
+    $query = $_[0];
+
+    $query_handle = $connect->prepare($query);
+    $query_handle->execute();
+
+    return $query_handle;
+}
+
+sub formatDate {
+
+    $date = $_[0];
+
+    $date =~ s/\n//g;
+    $newDate = '';
+    if($date =~ m/^[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]$/) {
+		@dateElements = split(/\//, $date);
+		$newDate = $dateElements[2] . '-' . $dateElements[1] . '-' . $dateElements[0];
+    }
+
+    return $date;
+}
+
+sub formatIrapNumber {
+
+    $irapNumber = $_[0];
+
+    if($irapNumber =~ m/^IRAP..-....$/) {
+		$irapNumber =~ s/IRAP/IRAP-/g;
+    }
+
+    return $irapNumber;
+}
+
+sub formatMontant {
+	
+	$montant = $_[0];
+	
+	if($montant eq "") {
+		return 0;
+	}
+	
+	$montant =~ s/ //g;
+	$montant =~ s/€//g;
+	$montant =~ s/,/./g;
+	
+	return $montant;
+}
 
 # CONFIG VARIABLES
 $platform = "mysql";
@@ -15,42 +85,18 @@ $port = "3306";
 $user = "root";
 $pw = "root";
 
-$tableName = "utilisateurs";
-
 # DATA SOURCE NAME
 $dsn = "dbi:$platform:$database:$host:$port";
 
 # PERL DBI CONNECT
-# $connect = DBI->connect($dsn, $user, $pw);
+$connect = DBI->connect($dsn, $user, $pw);
 
-# PREPARE THE QUERY
-# $query = "INSERT INTO $tableName (nom, role) VALUES ('Pierrick', 'Super Administrateur')";
-# $query_handle = $connect->prepare($query);
-
-# EXECUTE THE QUERY
-# $query_handle->execute();
-
-####
-# Select SQL requets
-####
+$csvFile = $ARGV[0];
+$line = '';
 
 
-
-
-
-
-
-
-
-
-####
-# Read file
-####
-
-my $fileName = "test_inventaire_IRAP_2012_V5.csv";
-
-open(my $FH, '<', $fileName) or die "Can't open $fileName: $!";
-while (my $line = readline($FH)) {
+open($FH, '<', $csvFile) or die "Can't open $csvFile: $!";
+while ($line = readline($FH)) {
 
     $line =~ s/"//g;
     @values = split(/;/, $line);
@@ -79,20 +125,53 @@ while (my $line = readline($FH)) {
     $mailResponsable = $values[21];
     $date = $values[22];
 
-    # print "$designation $categorie $sousCategorie $irapNumber $description $organisme $inventoriable $technique $status $fournisseur $montant $credit $cde $comptable $refMateriel $groupeThematique $groupeTechnique $numeroInventaire $lieuStockage $descriptionLieu $nomResponsable $mailResponsable $date \n";
+    $formatedDate = formatDate($date);
+    $formatedIrapNumber = formatIrapNumber($irapNumber);
+    $formatedMontant = formatMontant($montant);
+    $formatedMaterielAdministratif = formatMaterielAdministratif($technique);
+    $formatedMaterielTechnique = formatMaterielTechnique($technique);
+    
+    $idCategorie = '';
+    $idSousCategorie = '';
 
-    # Search the date format : jj/mm/aaaa
-    $date =~ s/\n//g;
-    $newDate;
-    if($date =~ m/^[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]$/) {
-	@dateElements = split(/\//, $date);
-	$newDate = $dateElements[2] . '-' . $dateElements[1] . '-' . $dateElements[0];
-	print "My new date is : $newDate\n";
+    if(!$categorie eq "") {
+
+		$query_handle = sqlRequest("SELECT id, nom FROM categories WHERE nom = \"$categorie\"");
+		$query_handle->bind_columns(undef, \$idCategorie, \$nomCategorie);
+
+		if(!$query_handle->fetch()) {
+
+			sqlRequest("INSERT INTO categories (nom) VALUES (\"$categorie\")");
+			$query_handle = sqlRequest("SELECT id, nom FROM categories WHERE nom = \"$categorie\"");
+			$query_handle->bind_columns(undef, \$idCategorie, \$nomCategorie);
+			$query_handle->fetch();
+
+		}
     } else {
-	print "Failed\n";
-    }
+		$idCategorie = '45';
+	}
 
+    if(!$sousCategorie eq "") {
 
-    print "My ultimate date = $newDate\n";
+		$query_handle = sqlRequest("SELECT id, nom, categorie_id FROM sous_categories WHERE nom = \"$sousCategorie\"");
+		$query_handle->bind_columns(undef, \$idSousCategorie, \$nomSousCategorie, \$refCategorieId);
+
+		if(!$query_handle->fetch()) {
+
+			sqlRequest("INSERT INTO sous_categories (nom, categorie_id) VALUES (\"$sousCategorie\", $idCategorie)");
+			$query_handle = sqlRequest("SELECT id, nom, categorie_id FROM sous_categories WHERE nom = \"$sousCategorie\"");
+			$query_handle->bind_columns(undef, \$idSousCategorie, \$nomSousCategorie, \$refCategorieId);
+			$query_handle->fetch();
+
+		}
+	} else {
+		$idSousCategorie = '99';
+	}
+	
+	$request = "INSERT INTO materiels (designation, categorie_id, sous_categorie_id, numero_irap, description, organisme, materiel_administratif, materiel_technique, status, date_acquisition, fournisseur, prix_ht, eotp, numero_commande, code_comptable, numero_serie, groupe_thematique_id, groupe_technique_id, ref_existante, lieu_stockage, lieu_detail, nom_responsable, email_responsable) VALUES (\"$designation\", $idCategorie, $idSousCategorie, \"$formatedIrapNumber\", \"$description\", \"$organisme\", $formatedMaterielAdministratif, $formatedMaterielTechnique, \"$status\", \"$formatedDate\", \"$fournisseur\", $formatedMontant, \"$credit\", \"$cde\", \"$comptable\", \"$refMateriel\", NULL, NULL, \"$numeroInventaire\", \"$lieuStockage\", \"$descriptionLieu\", \"$nomResponsable\", \"$mailResponsable\" )";
+	
+	# print $request . "\n";
+	
+	sqlRequest($request);
 }
-close($FH);
+

d07a4d59b6c9e6ad85d4221b028323fadead8bd7
Thomas Zilio <zilio_thomas@yahoo.fr>
2012-07-12 17:13:49 +0200

    #18 code refactoring

--- iOS/Inventirap/Inventirap/ScannerViewController.mm
+++ iOS/Inventirap/Inventirap/ScannerViewController.mm
@@ -13,6 +13,8 @@
 #import "Settings.h"
 #import "Product.h"
 
+#define kBgQueue dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+
 @interface ScannerViewController ()
 
 @property (nonatomic, retain) InformationViewController *informationViewController;
@@ -23,10 +25,15 @@
 
 - (void)launchQRCodeReader;
 - (void)processResults;
+- (void) parseJsonData;
+
 - (void)sendWebServiceRequest:(NSString*)ident;
-- (void)createSection:(NSString*)section From:(NSDictionary*)dictionary ForProduct:(Product*)product;
-- (void)createSections:(NSString*)sectionsName From:(NSArray*)array For:(Product*)product;
+
+- (void)createSection:(NSString*)section From:(NSDictionary*)dictionary withKey:(NSString*)key ForProduct:(Product*)product;
+- (void)createSections:(NSString*)sectionsName From:(NSDictionary*)dictionnary withKey:(NSString*)key For:(Product*)product;
+- (void) createProperty:(NSString*)propertyName From:(NSDictionary*)dictionnary withKey:(NSString*)key ForProduct:(Product*)product;
 - (NSString*) checkValueValidity:(id)value;
+
 - (void)customizeButtonLayer:(CALayer*)layer;
 
 @end
@@ -169,6 +176,75 @@
     
 }
 
+- (void) parseJsonData
+{
+#warning Replace with real json data
+    NSString *blabla = @"{\"materials\":[{\"Materiel\":{\"id\":\"1\",\"designation\":\"Macbook air\",\"category_id\":\"1\",\"sous_category_id\":\"2\",\"numero_irap\":\"IRAP-12-0001\",\"description\":\"Ceci est une description un peu nulle\",\"organisme\":\"IRAP\",\"materiel_administratif\":false,\"nom_responsable\":\"Cedric\",\"email_responsable\":\"Cedric.Hillembrand@irap.omp.eu\",\"materiel_technique\":false,\"status\":\"CREATED\",\"date_acquisition\":\"2012-07-04\",\"fournisseur\":\"Apple\",\"prix_ht\":\"1000\",\"eotp\":\"WTF\",\"numero_commande\":\"ZERZE45\",\"code_comptable\":\"44\",\"numero_serie\":null,\"thematic_group_id\":\"1\",\"work_group_id\":\"1\",\"ref_existante\":null,\"lieu_stockage\":\"B\",\"lieu_detail\":\"Chambre\",\"utilisateur_id\":\"1\",\"full_storage\":\"B-Chambre\"},\"Category\":{\"id\":\"1\",\"nom\":\"Multimetre\"},\"SousCategory\":{\"id\":\"2\",\"nom\":\"RUI + LC\",\"category_id\":\"1\"},\"ThematicGroup\":{\"id\":\"1\",\"nom\":\"GPPS\"},\"WorkGroup\":{\"id\":\"1\",\"nom\":\"NVA\"},\"Suivi\":[{\"id\":\"1\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Maintenance\",\"organisme\":\"IRAP\",\"frequence\":\"3\",\"commentaire\":\"Super cooolos\"},{\"id\":\"2\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Calibration\",\"organisme\":\"IRAP\",\"frequence\":\"1\",\"commentaire\":\"Ca sert pas \u00e0 grand chose\"},{\"id\":\"3\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Maintenance\",\"organisme\":\"IRAP\",\"frequence\":\"10\",\"commentaire\":\"Pas souvent lui la maintenance\"}],\"Emprunt\":[{\"id\":\"1\",\"materiel_id\":\"1\",\"date_emprunt\":\"2011-01-01\",\"date_retour_emprunt\":\"2013-01-01\",\"piece\":\"Souris\",\"emprunt_interne\":false,\"laboratoire\":\"IRAP\",\"responsable\":\"Dark Vador\"},{\"id\":\"2\",\"materiel_id\":\"1\",\"date_emprunt\":\"2010-04-05\",\"date_retour_emprunt\":\"2010-12-12\",\"piece\":\"Clavier\",\"emprunt_interne\":false,\"laboratoire\":\"IRAP\",\"responsable\":\"Woody Allen\"}]}],\"id\":\"IRAP-12-0001\"}";
+    
+    NSData *jsonDatax = [blabla dataUsingEncoding:NSUTF8StringEncoding];
+    
+    @try {
+        NSError *error = nil;
+        NSDictionary *res = [NSJSONSerialization JSONObjectWithData:jsonDatax options:kNilOptions error:&error];
+        
+        if (res == NULL) {
+            [NSException raise:@"Invalid web service response" format:@"json results are incorrect : %@", res];
+        }
+        
+        Product *simpleProduct = [[Product alloc] init];
+        Product *detailedProduct = [[Product alloc] init];
+        
+        NSArray *results = [res objectForKey:@"materials"];
+        NSDictionary* result = [results objectAtIndex:0];
+        
+        // Creating our simple product
+        NSDictionary* materiel = [result objectForKey:@"Materiel"];
+        
+        [self createProperty:NSLocalizedString(@"DESIGNATION", nil) From:materiel withKey:@"designation" ForProduct:simpleProduct];
+        [self createProperty:NSLocalizedString(@"NUMIRAP", nil) From:materiel withKey:@"numero_irap" ForProduct:simpleProduct];
+        [self createProperty:NSLocalizedString(@"PURCHASINGORGA", nil) From:materiel withKey:@"organisme" ForProduct:simpleProduct];
+        [self createProperty:NSLocalizedString(@"ACCOUNTANT", nil) From:materiel withKey:@"nom_responsable" ForProduct:simpleProduct];
+        [self createProperty:NSLocalizedString(@"ACCOUNTCONTACT", nil) From:materiel withKey:@"email_responsable" ForProduct:simpleProduct];
+        [self createProperty:NSLocalizedString(@"LOCALIZATION", nil) From:materiel withKey:@"full_storage" ForProduct:simpleProduct];
+        
+        [simpleProduct setSectionWithName:NSLocalizedString(@"MATERIAL", nil)];
+        
+        
+        //Creating a more detailed product
+        [self createSection:NSLocalizedString(@"MATERIAL", nil) From:result withKey:@"Materiel" ForProduct:detailedProduct];
+        [self createSection:NSLocalizedString(@"CATEGORY", nil) From:result withKey:@"Category" ForProduct:detailedProduct];
+        [self createSection:NSLocalizedString(@"SUBCATEGORY", nil) From:result withKey:@"SousCategory" ForProduct:detailedProduct];
+        [self createSection:NSLocalizedString(@"THEMATICGROUP", nil) From:result withKey:@"ThematicGroup" ForProduct:detailedProduct];
+        [self createSection:NSLocalizedString(@"WORKGROUP", nil) From:result withKey:@"WorkGroup" ForProduct:detailedProduct];
+        
+        [self createSections:NSLocalizedString(@"BORROWING", nil) From:result withKey:@"Emprunt" For:detailedProduct];
+        [self createSections:NSLocalizedString(@"MONITORING", nil) From:result withKey:@"Suivi" For:detailedProduct];
+        
+#warning Remove this command
+        [NSThread sleepForTimeInterval:5];
+        
+        // Setting the information view
+        [[self informationViewController] setSimpleProduct:simpleProduct];
+        [[self informationViewController] setDetailedProduct:detailedProduct];
+        [[self informationViewController] displaySimpleProduct];
+        
+        [[[self informationViewController] tableView] scrollRectToVisible:CGRectMake(0, 0, 1, 1) animated:NO];
+        [[[self informationViewController] navigationItem] setTitle : [simpleProduct name]];
+        
+        [self.navigationController pushViewController:[self informationViewController] animated:TRUE];
+        [[self informationLabel] setHidden:YES];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"main: Caught %@: %@", [exception name], [exception reason]);
+        [[self informationLabel] setTextColor: [UIColor redColor]];
+        [[self informationLabel] setText:NSLocalizedString(@"ERRORPARSINGRES", nil)];
+    }
+    @finally {
+        [[self applicationActivity] stopAnimating];
+        [[self scanButton] setEnabled:true];
+    }
+}
+
 #pragma mark -
 #pragma mark ZXing delegate methods
 
@@ -211,13 +287,17 @@
     
 }
 
-- (void)createSection:(NSString*)sectionName From:(NSDictionary*)dictionary ForProduct:(Product*)product
+- (void)createSection:(NSString*)sectionName From:(NSDictionary*)dictionary withKey:(NSString*)key ForProduct:(Product*)product
 {
+    if (key != nil)
+        dictionary = [dictionary objectForKey:key];
+    NSString *keyAsString;
+    NSString *valueAsString;
+    
     for(id key in dictionary) {
         id value = [dictionary objectForKey:key];
-        
-        NSString *keyAsString = [NSString stringWithFormat:@"%@",key];
-        NSString *valueAsString = [self checkValueValidity:value];
+        keyAsString = [NSString stringWithFormat:@"%@",key];
+        valueAsString = [self checkValueValidity:value];
         
         if ([keyAsString isEqualToString:@"designation"]) {
             [product setName:valueAsString];
@@ -228,12 +308,24 @@
     [product setSectionWithName:sectionName];
 }
 
-- (void)createSections:(NSString*)sectionsName From:(NSArray*)array For:(Product*)product
+- (void)createSections:(NSString*)sectionsName From:(NSDictionary*)dictionnary withKey:(NSString*)key For:(Product*)product
 {
+    NSArray *array = [dictionnary objectForKey:key];
     int i = 1;
     for (NSDictionary *element in array) {
-        [self createSection:[NSString stringWithFormat:@"%@ %d", sectionsName, i] From:element ForProduct:product];
+        [self createSection:[NSString stringWithFormat:@"%@ %d", sectionsName, i] From:element withKey:nil ForProduct:product];
         i++;
+    }
+}
+
+- (void) createProperty:(NSString*)propertyName From:(NSDictionary*)dictionnary withKey:(NSString*)key ForProduct:(Product*)product
+{
+    id value = [dictionnary objectForKey:key];
+    NSString *valueAsString = [self checkValueValidity:value];
+    [product addPropertyName:propertyName AndValue:valueAsString];
+    
+    if ([key isEqualToString:@"designation"]) {
+        [product setName:valueAsString];
     }
 }
 
@@ -281,109 +373,12 @@
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
     [[self informationLabel] setText:NSLocalizedString(@"PARSINGRES", nil)];
+    NSLog(@"%@", [[self informationLabel] text]);
     
-#warning Replace with real json data
-    NSString *blabla = @"{\"materials\":[{\"Materiel\":{\"id\":\"1\",\"designation\":\"Macbook air\",\"category_id\":\"1\",\"sous_category_id\":\"2\",\"numero_irap\":\"IRAP-12-0001\",\"description\":\"Ceci est une description un peu nulle\",\"organisme\":\"IRAP\",\"materiel_administratif\":false,\"nom_responsable\":\"Cedric\",\"email_responsable\":\"Cedric.Hillembrand@irap.omp.eu\",\"materiel_technique\":false,\"status\":\"CREATED\",\"date_acquisition\":\"2012-07-04\",\"fournisseur\":\"Apple\",\"prix_ht\":\"1000\",\"eotp\":\"WTF\",\"numero_commande\":\"ZERZE45\",\"code_comptable\":\"44\",\"numero_serie\":null,\"thematic_group_id\":\"1\",\"work_group_id\":\"1\",\"ref_existante\":null,\"lieu_stockage\":\"B\",\"lieu_detail\":\"Chambre\",\"utilisateur_id\":\"1\",\"full_storage\":\"B-Chambre\"},\"Category\":{\"id\":\"1\",\"nom\":\"Multimetre\"},\"SousCategory\":{\"id\":\"2\",\"nom\":\"RUI + LC\",\"category_id\":\"1\"},\"ThematicGroup\":{\"id\":\"1\",\"nom\":\"GPPS\"},\"WorkGroup\":{\"id\":\"1\",\"nom\":\"NVA\"},\"Suivi\":[{\"id\":\"1\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Maintenance\",\"organisme\":\"IRAP\",\"frequence\":\"3\",\"commentaire\":\"Super cooolos\"},{\"id\":\"2\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Calibration\",\"organisme\":\"IRAP\",\"frequence\":\"1\",\"commentaire\":\"Ca sert pas \u00e0 grand chose\"},{\"id\":\"3\",\"materiel_id\":\"1\",\"date_controle\":\"2012-03-03\",\"date_prochain_controle\":\"2012-03-03\",\"type_intervention\":\"Maintenance\",\"organisme\":\"IRAP\",\"frequence\":\"10\",\"commentaire\":\"Pas souvent lui la maintenance\"}],\"Emprunt\":[{\"id\":\"1\",\"materiel_id\":\"1\",\"date_emprunt\":\"2011-01-01\",\"date_retour_emprunt\":\"2013-01-01\",\"piece\":\"Souris\",\"emprunt_interne\":false,\"laboratoire\":\"IRAP\",\"responsable\":\"Dark Vador\"},{\"id\":\"2\",\"materiel_id\":\"1\",\"date_emprunt\":\"2010-04-05\",\"date_retour_emprunt\":\"2010-12-12\",\"piece\":\"Clavier\",\"emprunt_interne\":false,\"laboratoire\":\"IRAP\",\"responsable\":\"Woody Allen\"}]}],\"id\":\"IRAP-12-0001\"}";
-    
-    NSData *jsonDatax = [blabla dataUsingEncoding:NSUTF8StringEncoding];
-    
-    @try {
-        NSError *error = nil;
-        NSDictionary *res = [NSJSONSerialization JSONObjectWithData:jsonDatax options:kNilOptions error:&error];
-        
-        if (res == NULL) {
-            [NSException raise:@"Invalid web service response" format:@"json results are incorrect : %@", res];
-        }
-        
-        Product *simpleProduct = [[Product alloc] init];
-        Product *detailedProduct = [[Product alloc] init];
-        
-        NSArray *results = [res objectForKey:@"materials"];
-        NSDictionary* result = [results objectAtIndex:0];
-        
-        NSString *valueAsString;
-        id value;
-        
-        // Creating our simple product
-        NSDictionary* materiel = [result objectForKey:@"Materiel"];
-        
-        value = [materiel objectForKey:@"designation"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"DESIGNATION", nil) AndValue:valueAsString];
-        [simpleProduct setName:valueAsString];
-        
-        value = [materiel objectForKey:@"numero_irap"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"NUMIRAP", nil) AndValue:valueAsString];
-        
-        value = [materiel objectForKey:@"organisme"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"PURCHASINGORGA", nil) AndValue:valueAsString];
-        
-        value = [materiel objectForKey:@"nom_responsable"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"ACCOUNTANT", nil) AndValue:valueAsString];
-        
-        value = [materiel objectForKey:@"email_responsable"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"ACCOUNTCONTACT", nil) AndValue:valueAsString];
-        
-        value = [materiel objectForKey:@"full_storage"];
-        valueAsString = [self checkValueValidity:value];
-        [simpleProduct addPropertyName:NSLocalizedString(@"LOCALIZATION", nil) AndValue:valueAsString];
-        
-        [simpleProduct setSectionWithName:NSLocalizedString(@"MATERIAL", nil)];
-        
-        
-        //Creating a more detailed product
-         [self createSection:NSLocalizedString(@"MATERIAL", nil) From:materiel ForProduct:detailedProduct];
-
-        // Parsing Category
-        NSDictionary* category = [result objectForKey:@"Category"];
-        [self createSection:NSLocalizedString(@"CATEGORY", nil) From:category ForProduct:detailedProduct];
-        
-        // Parsing SousCategory
-        NSDictionary* subcategory = [result objectForKey:@"SousCategory"];
-        [self createSection:NSLocalizedString(@"SUBCATEGORY", nil) From:subcategory ForProduct:detailedProduct];
-
-        // Parsing Thematic group
-        NSDictionary *thematicGroup = [result objectForKey:@"ThematicGroup"];
-        [self createSection:NSLocalizedString(@"THEMATICGROUP", nil) From:thematicGroup ForProduct:detailedProduct];
-        
-        // Parsing Work group
-        NSDictionary *workGroup = [result objectForKey:@"WorkGroup"];
-        [self createSection:NSLocalizedString(@"WORKGROUP", nil) From:workGroup ForProduct:detailedProduct];
-        
-        // Parsing borrowings
-        NSArray *borrowings = [result objectForKey:@"Emprunt"];
-        [self createSections:NSLocalizedString(@"BORROWING", nil) From:borrowings For:detailedProduct];
-        
-        // Parsing borrowings
-        NSArray *monitorings = [result objectForKey:@"Suivi"];
-        [self createSections:NSLocalizedString(@"MONITORING", nil) From:monitorings For:detailedProduct];
-        
-        //[NSThread sleepForTimeInterval:2];
-        
-        // Setting the information view
-        [[self informationViewController] setSimpleProduct:simpleProduct];
-        [[self informationViewController] setDetailedProduct:detailedProduct];
-        [[self informationViewController] displaySimpleProduct];
-        
-        [[[self informationViewController] tableView] scrollRectToVisible:CGRectMake(0, 0, 1, 1) animated:NO];
-        [[[self informationViewController] navigationItem] setTitle : [simpleProduct name]];
-        
-        [self.navigationController pushViewController:[self informationViewController] animated:TRUE];
-        [[self informationLabel] setHidden:YES];
-    }
-    @catch (NSException *exception) {
-        NSLog(@"main: Caught %@: %@", [exception name], [exception reason]);
-        [[self informationLabel] setTextColor: [UIColor redColor]];
-        [[self informationLabel] setText:NSLocalizedString(@"ERRORPARSINGRES", nil)];
-    }
-    @finally {
-        [[self applicationActivity] stopAnimating];
-        [[self scanButton] setEnabled:true];
-    }
+    dispatch_async(kBgQueue, ^{
+        [self performSelectorOnMainThread:@selector(parseJsonData) 
+                               withObject:nil waitUntilDone:YES];
+    });
 }
 
 - (NSCachedURLResponse *)connection:(NSURLConnection *)connection willCacheResponse:(NSCachedURLResponse *)cachedResponse

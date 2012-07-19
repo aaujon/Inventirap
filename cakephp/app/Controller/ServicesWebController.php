5315afe3de3575d7344370a1269d8c05c65fc5fa
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-07-19 09:18:17 +0200

    Authenticated web service. Currently, it doesn't work, I need the key for the decrypt_password function. The web service works with the clear password !

--- cakephp/app/Controller/ServicesWebController.php
+++ cakephp/app/Controller/ServicesWebController.php
@@ -1,20 +1,40 @@
 <?php
 class ServicesWebController extends AppController {
-	
+
 	var $layout = 'xml';
-	
+
 	public function beforeFilter() {
 		$this->LdapAuth->allow('*');
-    }
-	
-	public function materiel($irapNum = '') {
-		if(preg_match('~IRAP-..-[0-9]*~', $irapNum)) {
-			$materiel = ClassRegistry::init('Materiel')->
+	}
+
+	public function materiel($irapNum = '', $login = '', $passwordAES128 = '') {
+		if(ClassRegistry::init('LdapConnection')->ldapAuthentication($login, $passwordAES128)) {
+
+			if(preg_match('~IRAP-..-[0-9]*~', $irapNum)) {
+				$materiel = ClassRegistry::init('Materiel')->
 				find('all', array('conditions' => array('numero_irap' => $irapNum)));
-                    
-			$this->set('id', $irapNum);
-			$this->set('materials', $materiel);
+
+				$this->set('login', $login);
+				$this->set('id', $irapNum);
+				$this->set('materials', $materiel);
+			}
 		}
+	}
+
+	private function decrypt_password($pass,$key)
+	{
+
+		$base64encoded_ciphertext = $pass;
+
+		$res_non = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($base64encoded_ciphertext), ‘ecb’);
+
+		$decrypted = $res_non;
+		$dec_s2 = strlen($decrypted);
+
+		$padding = ord($decrypted[$dec_s2-1]);
+		$decrypted = substr($decrypted, 0, -$padding);
+
+		return  $decrypted;
 	}
 }
 

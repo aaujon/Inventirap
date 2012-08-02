d3a958595b50acb3362543e2600e706a54ebf7de
Pierrick MARIE <pierrick.marie1456@gmail.com>
2012-08-02 10:16:27 +0200

    Show an error message if the authentication for the web service fail.

--- cakephp/app/Controller/ServicesWebController.php
+++ cakephp/app/Controller/ServicesWebController.php
@@ -18,6 +18,8 @@ class ServicesWebController extends AppController {
 				$this->set('id', $irapNum);
 				$this->set('materials', $materiel);
 			}
+		} else { 
+			$this->set('message', 'Erreur d\'authentification');
 		}
 	}
 }
